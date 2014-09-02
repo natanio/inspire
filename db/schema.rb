6a2c086046a09707f07032804f73cb2814868d9a
Nate Hill <natanio@gmail.com>
2014-09-03 06:47:32 +0900

    Add user association to books

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140902212634) do
+ActiveRecord::Schema.define(version: 20140902214259) do
 
   create_table "books", force: true do |t|
     t.string   "title"
@@ -22,7 +22,10 @@ ActiveRecord::Schema.define(version: 20140902212634) do
     t.string   "date_publish"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "books", ["user_id"], name: "index_books_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
