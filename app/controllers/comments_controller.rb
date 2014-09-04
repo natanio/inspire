4634489fdbd23f3b337375a59442474dc58ed22f
Nate Hill <natanio@gmail.com>
2014-09-04 12:34:24 +0900

    Get lots of the routes working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -33,7 +33,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to book_inspiration_path(@book,@inspiration), notice: 'Comment was successfully created.' }
         format.json { render action: 'show', status: :created, location: @comment }
       else
         format.html { render action: 'new' }
@@ -61,7 +61,7 @@ class CommentsController < ApplicationController
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url }
+      format.html { redirect_to @book }
       format.json { head :no_content }
     end
   end
--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,7 @@
 module BooksHelper
+	def comment_number
+		comment_count = []
+		comment_count << (Comment.find_by inspiration_id: 1)
+		return comment_count.length
+	end
 end
