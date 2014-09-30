7c13a2391d0f2d1b12a65e185b5725ec083b3360
Nate Hill <natanio@gmail.com>
2014-09-30 16:12:38 +0900

    Add partials for book, inspiration, modal

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,18 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :inspiration
+
+	delegate :book, :to => :inspiration, :prefix => true
+ 
+	def to_edit_path
+	  Rails.application.routes.url_helpers.edit_book_inspiration_comment_path inspiration_book, inspiration, self
+	end
+ 
+	def to_path
+	  Rails.application.routes.url_helpers.book_inspiration_comment_path inspiration_book, inspiration, self
+	end
+ 
+	def serializable_hash(*args)
+	  super.merge 'path' => to_path, 'edit_path' => to_edit_path
+	end
 end
--- app/models/inspiration.rb
+++ app/models/inspiration.rb
@@ -2,4 +2,6 @@ class Inspiration < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :book
 	has_many :comments
+
+	delegate :title, :link, :to => :book, :prefix => true
 end
