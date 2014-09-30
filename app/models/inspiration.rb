eb852d536d7051d87cafa526ed0d98ae6628347f
Nate Hill <natanio@gmail.com>
2014-09-30 23:00:46 +0900

    A bunch of shit to get the comments working in modals

--- app/models/inspiration.rb
+++ app/models/inspiration.rb
@@ -4,4 +4,12 @@ class Inspiration < ActiveRecord::Base
 	has_many :comments
 
 	delegate :title, :link, :to => :book, :prefix => true
+
+	def new_comment_path
+	  Rails.application.routes.url_helpers.new_book_inspiration_comment_path(book, self)
+	end
+ 
+	def serializable_hash(*args)
+	  super.merge 'new_comment_path' => new_comment_path
+	end
 end
