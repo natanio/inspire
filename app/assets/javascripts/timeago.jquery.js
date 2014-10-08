fa8c1828afccb18a8ae8fd60e8f5a5573e1179c2
Nate Hill <natanio@gmail.com>
2014-10-09 07:10:55 +0900

    Add remote to comments form and js handler file for create

--- app/assets/javascripts/timeago.jquery.js
+++ app/assets/javascripts/timeago.jquery.js
@@ -13,7 +13,7 @@
  *
  * Copyright (c) 2008-2013, Ryan McGeary (ryan -[at]- mcgeary [*dot*] org)
  */
-
+/*
 (function (factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
