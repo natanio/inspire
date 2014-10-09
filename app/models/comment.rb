9b7009c9e5315b1f2960d09c5f49715c2a3f24b3
Nate Hill <natanio@gmail.com>
2014-10-10 06:43:33 +0900

    Edit comment js for ajax submission

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,8 @@ class Comment < ActiveRecord::Base
 	belongs_to :inspiration
 
 	delegate :book, :to => :inspiration, :prefix => true
+
+	validates :content, presence: true
  
 	def to_edit_path
 	  Rails.application.routes.url_helpers.edit_book_inspiration_comment_path inspiration_book, inspiration, self
