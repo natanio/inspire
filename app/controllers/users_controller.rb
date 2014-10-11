4757a17f2a8d18799b150609bc6226f5be168b49
Nate Hill <natanio@gmail.com>
2014-10-11 17:16:01 +0900

    User profile now has link to favorites, book variable still not passing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
+
 	def show
     @user = User.find(params[:id])
     @user_favorites = @user.favorite_inspirations
-    @book = Book.where(book_id: @user_favorites)
+    @book = Book.find_by(:id, @inspiration)
 
     respond_to do |format|
         format.html # show.html.erb
         format.xml { render :xml => @user }
     end
   end
+
+  private
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	has_many :inspirations
 	has_many :comments, through: :inspirations
+	has_many :favorites, through: :inspirations
 
 	searchkick
 
